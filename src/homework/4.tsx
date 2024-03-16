2e60f44df37c25fea483df1f9fbedc0210bda236
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2024-03-16 11:07:33 -0500

    done working

--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -34,7 +34,9 @@ type PropsProvider = {
 
 function MenuProvider({ children }: PropsProvider) {
   // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+  const [selectedMenu, setSelectedMenu] = useState<{ SelectedMenu: string }>({
+    id,
+  });
 
   const menuContextAction = useMemo(
     () => ({
